f95500da8bca5ab738488c5c88a9ba64638545da
codingarmy45 <135152967+codingarmy45@users.noreply.github.com>
2024-11-05 09:39:35 +0530

    Add files via upload

---
+++ BT/02_pr.sol
@@ -0,0 +1,32 @@
+// Solidity version 0.8.0 or above is recommended for added security
+pragma solidity ^0.8.0;
+
+// Creating a contract named StudentData
+contract StudentData {
+    // Defining a structure to store individual student information
+    struct Student {
+        uint id;
+        string name;
+        uint age;
+        string course;
+    }
+
+    // Mapping to store student information by ID
+    mapping(uint => Student) private students;
+
+    // Event to log student creation
+    event StudentCreated(uint id, string name, uint age, string course);
+
+    // Function to add a student to the contract
+    function addStudent(uint _id, string memory _name, uint _age, string memory _course) public {
+        // Create a new student and store it in the mapping
+        students[_id] = Student(_id, _name, _age, _course);
+        emit StudentCreated(_id, _name, _age, _course);
+    }
+
+    // Function to retrieve student information by ID
+    function getStudent(uint _id) public view returns (uint, string memory, uint, string memory) {
+        Student memory student = students[_id];
+        return (student.id, student.name, student.age, student.course);
+    }
+}
